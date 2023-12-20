dfd767d9947bec9ec0ab1031bd729aec8f814a23
Ala Trojette <ala.trojette35@gmail.com>
2023-12-20 13:13:12 +0100

    Json file added

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,31 +1,114 @@
 package dictionary
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
 type Dictionary struct {
-	translations map[string]string
+	filePath string
+}
+type Data struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
 }
 
-func NewDictionary() Dictionary {
+func NewDictionary(filePath string) Dictionary {
 	return Dictionary{
-		translations: make(map[string]string),
+		filePath: filePath,
 	}
 }
 
-func (d *Dictionary) Get(key string) string {
-	return d.translations[key]
-}
-
-func (d *Dictionary) Remove(key string) {
-	delete(d.translations, key)
-}
-
-func (d *Dictionary) List() []string {
-	result := make([]string, 0, len(d.translations))
-	for key, value := range d.translations {
-		result = append(result, key+": "+value+",")
+func (d *Dictionary) Get(key string) (string, error) {
+	data, err := os.ReadFile(d.filePath)
+	if err != nil {
+		return "", err
 	}
-	return result
+
+	var jsonData map[string]string
+	if err := json.Unmarshal(data, &jsonData); err != nil {
+		return "", err
+	}
+
+	value, exists := jsonData[key]
+	if !exists {
+		return "", errors.New("key not found")
+	}
+
+	return value, nil
 }
 
-func (d *Dictionary) Add(key, value string) {
-	d.translations[key] = value
+func (d *Dictionary) Remove(key string) error {
+	file, err := os.ReadFile(d.filePath)
+	if err != nil {
+		return err
+	}
+
+	var jsonData map[string]string
+	if err := json.Unmarshal(file, &jsonData); err != nil {
+		return err
+	}
+
+	// Check if the key exists
+	if _, exists := jsonData[key]; !exists {
+		return errors.New("key not found")
+	}
+
+	delete(jsonData, key)
+
+	updatedData, err := json.MarshalIndent(jsonData, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(d.filePath, updatedData, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("removed successfully!")
+	return nil
+}
+
+func (d Dictionary) Add(key, value string) error {
+	dataMap := make(map[string]string)
+	if existingData, err := os.ReadFile(d.filePath); err == nil && len(existingData) > 0 {
+		if err := json.Unmarshal(existingData, &dataMap); err != nil {
+			return err
+		}
+	}
+	dataMap[key] = value
+
+	updatedData, err := json.MarshalIndent(dataMap, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(d.filePath, updatedData, 0644)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(" added successfully!")
+	return nil
+}
+func (d *Dictionary) List() ([]string, error) {
+	data, err := os.ReadFile(d.filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var jsonData map[string]string
+	if err := json.Unmarshal(data, &jsonData); err != nil {
+		return nil, err
+	}
+
+	var result []string
+	for key, value := range jsonData {
+		result = append(result, fmt.Sprintf("%s: %s", key, value))
+	}
+
+	return result, nil
 }
--- main.go
+++ main.go
@@ -6,21 +6,26 @@ import (
 )
 
 func main() {
-	d := dictionary.NewDictionary()
+	filepath := "dictionary/dict.json"
+	d := dictionary.NewDictionary(filepath)
 
 	d.Add("francais", "french")
 	d.Add("anglais", "english")
 	d.Add("espagnol", "spanish")
 	fmt.Println(d)
 	d.Add("italien", "italian")
-	v1 := d.Get("anglais")
+	v1, _ := d.Get("anglais")
 
-	
 	fmt.Println("v1:", v1)
 
 	d.Remove("anglais")
-	
-	fmt.Println("List:", d.List())
+
+	list, err := d.List()
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("List:", list)
+	}
 
 	
 	
